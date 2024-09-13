b8c0dc9b0e1faf186cf5bb1b2b3ebf7d730a730b
Tiago Farinas <tiagofarinas@gmail.com>
2024-09-13 17:40:03 -0400

    Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -21,3 +21,13 @@ const Inventory =[
      quantity:30,
      lowStockLevel:3}
 ] //This array contains 5 objects that represent the products and each product information
+
+//Task 2-Create a Function to Display Product Details
+function displayProductDetails(product){
+    const Stock_status= product.quantity>=product.lowStockLevel?"In Stock":"Low Stock";
+    console.log(`Product Name: ${product.name}`);
+    console.log(`Price: $${product.price}`);
+    console.log(`Quantity in Stock: ${product.quantity}`);
+    console.log(`Stock Status: ${Stock_status}`);
+}
+
