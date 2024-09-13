d573869afa22f5e15d72442fe40f1906f2540112
Tiago Farinas <tiagofarinas@gmail.com>
2024-09-13 17:04:58 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,23 @@
+//Task 1 - Create an Inventory Array of Product Objects
+const Inventory =[
+    {name:"Computer",
+     price:100,
+     quantity:150,
+     lowStockLevel:15},
+    {name:"Tablet",
+     price:250,
+     quantity:200,
+     lowStockLevel:20},
+    {name:"AirPods",
+     price:90,
+     quantity:50,
+     lowStockLevel:5},
+    {name:"Monitor",
+     price:200,
+     quantity:60,
+     lowStockLevel:10},
+    {name:"PS4",
+     price:300,
+     quantity:30,
+     lowStockLevel:3}
+] //This array contains 5 objects that represent the products and each product information
