77917a42b07011cbdc312e8f58c1027c2387cdf8
Tiago Farinas <tiagofarinas@gmail.com>
2024-09-14 10:39:14 -0400

    Create updateStock function

--- inventory_management.js
+++ inventory_management.js
@@ -24,10 +24,22 @@ const Inventory =[
 
 //Task 2-Create a Function to Display Product Details
 function displayProductDetails(product){
-    const Stock_status= product.quantity>=product.lowStockLevel?"In Stock":"Low Stock";
+    var Stock_status= product.quantity>=product.lowStockLevel?"In Stock":"Low Stock";
     console.log(`Product Name: ${product.name}`);
     console.log(`Price: $${product.price}`);
     console.log(`Quantity in Stock: ${product.quantity}`);
     console.log(`Stock Status: ${Stock_status}`);
 }
+
+//Task 3-Create a Function to Update Product Stock After Sales
+function updateStock(product, UnitsSold){
+    if (product.quantity>= UnitsSold){
+        product.quantity -= UnitsSold;
+    if(product.quantity === 0){
+       return `${product.name} is out of stock` } 
+    else if (product.quantity<=product.lowStockLevel){
+        return `${product.name} stock is low`}
+    else{ return`${product.name} stock was updated`}
+    } else { return `${product.name} stock is not enough`}
+ }
 
