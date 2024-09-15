d22af1d1442cd8e33a2b38028aa1497eb857486a
Tiago Farinas <tiagofarinas@gmail.com>
2024-09-15 18:42:01 -0400

    corrections, review and comments

--- inventory_management.js
+++ inventory_management.js
@@ -23,13 +23,14 @@ const Inventory =[
 ] //This array contains 5 objects that represent the products and each product information
 
 //Task 2-Create a Function to Display Product Details
-function displayProductDetails(Inventory,product){
-    var Stock_status= product.quantity>=product.lowStockLevel?"In Stock":"Low Stock";
+function displayProductDetails(product){
+    var Stock_status = product.quantity >= product.lowStockLevel?"In Stock":"Low Stock";
     console.log(`Product Name: ${product.name}`);
     console.log(`Price: $${product.price}`);
     console.log(`Quantity in Stock: ${product.quantity}`);
     console.log(`Stock Status: ${Stock_status}`);
 }
+//This function accpects an product and log its propoerties
 
 //Task 3-Create a Function to Update Product Stock After Sales
 function updateStock(product, UnitsSold){
@@ -37,16 +38,17 @@ function updateStock(product, UnitsSold){
         product.quantity -= UnitsSold;
     if(product.quantity === 0){
        console.log(`${product.name} is out of stock` )}
-    else if (product.quantity<=product.lowStockLevel)
-        return `${product.name} stock is low`
-    else {console.log(`${product.name} stock was updted`)}
+    else if (product.quantity<=product.lowStockLevel){
+        console.log(`${product.name} stock is low`)}
+    else {console.log(`${product.name} stock was updated`)}
     } else { console.log(`${product.name} stock is not enough`)}
  }
+//This function will do the math when a product is sold and subtract the number of units sold from the stock
 
  //Task 4-Create a Function to Check Low Stock Products
  function checkLowStock(Inventory){
-    Inventory.forEach(product=> {
-        if (product.name<=product.lowStockLevel){
+    Inventory.forEach(product => {
+        if (product.quantity <= product.lowStockLevel){
             console.log(`${product.name} is low on stock`)
         }})};
 
@@ -59,7 +61,5 @@ function calculateInventoryValue(Inventory){
 function processSale(productName, unitsSold){
     const product = Inventory.find(item => item.name === productName);
     if(product){
-    updateStock(product, unitsSold)}
+    updateStock(product, unitsSold)} //Call
     else{ console.log(`${productName} was not found in inventory`)} };
-
-
